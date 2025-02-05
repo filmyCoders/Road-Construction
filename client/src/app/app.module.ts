226bc30408ba385f957ea6a8692d9216d33486a5
filmyCoders <filmycoders@gmail.com>
2025-02-05 17:11:51 +0530

    add all back-end

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SummaryViewComponent } from './component/summary-view/summary-view.component';
+import { DetailedViewComponent } from './component/detailed-view/detailed-view.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DetailedView1Component } from './component/detailed-view1/detailed-view1.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryViewComponent,
+    DetailedView1Component,
+    DetailedViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,  
+      HttpClientModule , // Ensure HttpClientModule is imported
+      FormsModule,
+      ReactiveFormsModule,
+      BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
